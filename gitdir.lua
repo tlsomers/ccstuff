2206166a0dbc2cb5c7d1291bb460212001bf997c
Thomas Somers <t.l.somers@student.tue.nl>
2020-07-02 22:59:09 +0200

    More stuff

--- gitdir.lua
+++ gitdir.lua
@@ -1,5 +1,6 @@
 local user = "tlsomers"
 local repo = "ccstuff"
+local cacheTime = 10
 
 local gitfs = {}
 
@@ -7,7 +8,7 @@ local gitfs = {}
 
 local cache = {}
 function http.getJSON(link, ...)
-  if cache[link] and cache[link].time > os.clock() - 5 then
+  if cache[link] and cache[link].time > os.clock() - cacheTime then
     return cache[link].response
   else
     local req, err = http.get(link, ...)
@@ -27,7 +28,7 @@ function gitfs.isReadOnly(fs)
 end
 
 function gitfs.list(_, path)
-  local link = "https://api.github.com/repos/"..user.."/"..repo.."/contents"
+  local link = "https://api.github.com/repos/"..user.."/"..repo.."/contents/"+path
 
   local req, err = http.getJSON(link)
   if not req then
@@ -38,6 +39,70 @@ function gitfs.list(_, path)
       items[#items+1] = v.path
     end
     return items
+  end
+end
+
+function gitfs.isDir(fs, path)
+  local parent = fs.getDir(path)
+  local link = "https://api.github.com/repos/"..user.."/"..repo.."/contents/"+parent
+  local req, err = http.getJSON(link)
+  for _,v in pairs(req) do
+    if v.name == fs.getName(path) then
+      return v.type == "dir"
+    end
+  end
+  return false
+end
+
+function gitfs.getSize(fs, path)
+  local parent = fs.getDir(path)
+  local link = "https://api.github.com/repos/"..user.."/"..repo.."/contents/"+parent
+  local req, err = http.getJSON(link)
+  for _,v in pairs(req) do
+    if v.name == fs.getName(path) then
+      return v.size
+    end
+  end
+  return 0
+end
+
+function gitfs.getDrive()
+  return "git"
+end
+
+function gitfs.getAttibutes(fs, path)
+  local parent = fs.getDir(path)
+  local link = "https://api.github.com/repos/"..user.."/"..repo.."/contents/"+parent
+  local req, err = http.getJSON(link)
+  for _,v in pairs(req) do
+    if v.name == fs.getName(path) then
+      return {
+        size = v.size,
+        isDir = v.type == "dir",
+        created = 0,
+        modified = 0
+      }
+    end
+  end
+  return {}}
+end
+
+function gitfs.open(fs, path, mode)fs.
+  if mode ~= "r" then
+    error("Cannot write to read only files")
+  else
+    local link = "https://api.github.com/repos/"..user.."/"..repo.."/contents/"+path
+    local r1, err = http.getJSON(link)
+    if not r1 then
+      return nil, path..": No such file"
+    else
+      local r2, err = http.get(r1.download_url)
+      if not r2 then
+        return nil, path..": Cannot open file"
+      else
+        return r2
+      end
+    end
   end
 end
 
