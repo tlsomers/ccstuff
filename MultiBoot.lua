5d38d059acb71611becc17fd532e13607a99d750
Thomas Somers <t.l.somers@student.tue.nl>
2020-07-03 16:36:42 +0200

    V2

--- MultiBoot.lua
+++ MultiBoot.lua
@@ -1,6 +1,70 @@
 
-dofile("pluggablefs.lua")
-dofile("symlink")
+function promptYN(text)
+  term.write(text .. " (y/n)")
+  while true do
+    local event, key = os.pullEvent( "key" )
+    if key == keys.y then
+      print()
+      return true
+    elseif key == keys.n then
+      print()
+      return false
+    end
+  end
+end
+
+local funtion getRawFile(link, path)
+  local file = fs.open(path, "w")
+  local handle, err = http.get(link)
+  if handle then
+    file.write(handle.readAll())
+    file.close()
+    handle.close()
+  else
+    error(err)
+  end
+end
+
+if not fs.exists(".multiboot") then
+  print("Multiboot is not installed")
+  if not promptYN("Install") then
+    return
+  end
+
+  fs.makeDir(".multiboot")
+
+  print("Downloading PluggableFs")
+  getRawFile("https://raw.githubusercontent.com/tlsomers/ccstuff/master/pluggablefs.lua", ".multiboot/pluggablefs.lua")
+
+  print("Downloading Symlink")
+  getRawFile("https://raw.githubusercontent.com/tlsomers/ccstuff/master/symlink", ".multiboot/symlink.lua")
+
+  print("Downloading bios")
+  getRawFile("https://raw.githubusercontent.com/SquidDev-CC/CC-Tweaked/7b2d4823879a6db77bb99fc2e8605e9e54a0d361/src/main/resources/data/computercraft/lua/bios.lua", ".multiboot/bios.lua")
+
+  print("Downloading MultiBoot")
+  getRawFile("https://raw.githubusercontent.com/tlsomers/ccstuff/master/MultiBoot.lua", "MultiBoot.lua")
+
+
+  print("Download completed")
+  if promptYN("Run at startup? (y/n)") then
+    if fs.exists("startup.lua") then
+      fs.copy("startup.lua", "startup_backup.lua")
+      print("Old startup saved as startup_backup.lua")
+    end
+    local file = fs.open("startup.lua", "w")
+    file.write("dofile('MultiBoot.lua')")
+    file.close()
+  end
+  if not promptYN("Run MultiBoot now?") then
+    return
+  end
+end
+
+local bios = loadfile(".multiboot/bios.lua")
+
+dofile(".multiboot/pluggablefs.lua")
+dofile(".multiboot/symlink.lua")
 
 term.clear()
 term.setCursorPos(1,1)
@@ -55,11 +119,7 @@ end
 fs.symlink(fs.combine(path, "rom"), "rom")
 
 print("Loading virtual file system")
-dofile("pluggablefs.lua")
-dofile("symlink")
 fs.symlink("/", path)
 
-print("Booting os")
-if fs.exists("startup.lua") then
-  dofile("startup.lua")
-end
+print("Booting OS")
+bios()
