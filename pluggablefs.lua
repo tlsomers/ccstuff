35ba68100603be4c9c6749cb3f1607cea4a83f27
Thomas Somers <t.l.somers@student.tue.nl>
2020-07-03 14:58:30 +0200

    upgrade

--- pluggablefs.lua
+++ pluggablefs.lua
@@ -7,151 +7,102 @@ end
 
 local oldfs = copyTable(_G.fs, {})
 
-local fs = {}
-
-local mounts = {}
-
 function clean(path)
   return oldfs.combine(path, "")
 end
 
-function fs.mount(path, mount)
-  path = oldfs.combine(path, "")
-  if not oldfs.isDir(path) then
-    error("Must mount in dir")
-  elseif oldfs.isReadOnly(path) then
-    error("Cannot mountin read-only place")
-  elseif getMount(path) then
-    error("Cannot mount in mount")
-  else
-    mounts[path] = mount
-  end
-end
-
-function getMount(path)
+local function getMount(mounts, path)
   path = clean(path)
-  for i,v in pairs(mounts) do
-    if path == i or path:sub(1,#i+1) == i .. "/" then
-      return {fs = v, path = path:sub(#i + 1), mount = i}
-    end
-  end
-  return false
-end
-
-function tryMount(func)
-  function inner (path, ...)
-    local mount = getMount(path)
-    if mount and mount.fs[func] then
-      return mount.fs[func](oldfs, mount.path, ...)
+  if #mounts == 0 then
+    return false
+  else
+    local initial = _.initial(mounts)
+    local last = _.last(mounts)
+    if last.path == "" or path == last.path or path:sub(1,#i+1) == last.path .. "/" then
+      return {fs = last.fs, path = path:sub(#i + 1), mount = last.path}, initial
     else
-      return oldfs[func](path, ...)
+      return getMounts(initial, path)
     end
-  end
-  return inner
 end
 
-fs.list = tryMount("list")
+local function fsWithMounts(mounts)
+  local fs = {}
 
-fs.getName = tryMount("getName")
-
-fs.getSize = tryMount("getSize")
-
-fs.exists = tryMount("exists")
-
-fs.isDir = tryMount("isDir")
-
-fs.isReadOnly = tryMount("isReadOnly")
-
-fs.makeDir = tryMount("makeDir")
-
-fs.delete = tryMount("delete")
-
-fs.open = tryMount("open")
-
-fs.getDrive = tryMount("getDrive")
-
-fs.getFreeSpace = tryMount("getFreeSpace")
-
-fs.getDir = tryMount("getDir")
-
-fs.isDriveRoot = tryMount("isDriveRoot")
-
-fs.getCapacity = tryMount("getCapacity")
-
-fs.attibutes = tryMount("attributes")
-
-function fs.combine(a,b)
-  return oldfs.combine(a,b)
-end
-
-local function moveOut(mount, dest, totalPath)
-  if mount.fs.moveOut then
-    return mount.fs.moveOut(oldfs, mount.path, dest)
-  else
-    return oldfs.move(totalPath, dest)
-  end
-end
-
-local function moveIn(mount, source, totalPath)
-  if mount.fs.moveIn then
-    return mount.fs.moveIn(oldfs, dest, mount.path)
-  else
-    return oldfs.move(source, totalPath)
-  end
-end
-
-function fs.move(patha, pathb)
-  patha = clean(patha)
-  pathb = clean(pathb)
-  local mounta = getMount(patha)
-  local mountb = getMount(pathb)
-  if not mounta and not mountb then
-    return oldfs.move(patha, pathb)
-  elseif mounta and mountb and mounta.fs == mountb.fs then
-    if mounta.fs.move then
-      return mounta.fs.move(oldfs, mounta.path, mountb.path)
-    else
-      return oldfs.move(patha, pathb)
+  function tryMount(func)
+    function inner (path, ...)
+      local mount, rest = getMount(mounts, path)
+      if mount then
+        local innerfs = fsWithMounts(rest)
+        if mount.fs[func] then
+          return mount.fs[func](innerfs, mount.path, ...)
+        else
+          return fsWithMounts(rest)[func](path, ...)
+        end
+      else
+        return oldfs[func](path, ...)
+      end
     end
-  elseif mounta and not mountb then
-    return moveOut(mounta, pathb, patha)
-  elseif mountb and not mounta then
-    return moveIn(mountb, patha, pathb)
-  else
-    local temp = makeTemp()
-    moveOut(mounta, temp, patha)
-    return moveIn(mountb, temp, pathb)
+    return inner
   end
+
+  fs.list = tryMount("list")
+
+  fs.getName = tryMount("getName")
+
+  fs.getSize = tryMount("getSize")
+
+  fs.exists = tryMount("exists")
+
+  fs.isDir = tryMount("isDir")
+
+  fs.isReadOnly = tryMount("isReadOnly")
+
+  fs.makeDir = tryMount("makeDir")
+
+  fs.delete = tryMount("delete")
+
+  fs.open = tryMount("open")
+
+  fs.getDrive = tryMount("getDrive")
+
+  fs.getFreeSpace = tryMount("getFreeSpace")
+
+  fs.getDir = tryMount("getDir")
+
+  fs.isDriveRoot = tryMount("isDriveRoot")
+
+  fs.getCapacity = tryMount("getCapacity")
+
+  fs.attibutes = tryMount("attributes")
+
+  function fs.combine(a,b)
+    return oldfs.combine(a,b)
+  end
+
+  fs.copy = oldfs.copy
+
+  fs.move = oldfs.move
+
+  fs.find = oldfs.find
+
+  fs.complete = oldfs.complete
+
+  return fs
 end
 
-function fs.copy (patha, pathb)
-  patha = clean(patha)
-  pathb = clean(pathb)
-  local mounta = getMount(patha)
-  local mountb = getMount(pathb)
-  if not mounta and not mountb then
-    oldfs.copy(patha, pathb)
-  elseif mounta and mountb and mounta.fs == mountb.fs then
-    if mounta.fs.copy then
-      return mounta.fs.copy(oldfs, mounta.path, mountb.path)
-    else
-      return oldfs.copy(patha, pathb)
-    end
+local allMounts = {}
+local fs = fsWithMounts(allMounts)
+
+
+function fs.mount(path, mount)
+  path = clean(path)
+  if not fs.isDir(path) then
+    error("Must mount in dir")
+  elseif fs.isReadOnly(path) then
+    error("Cannot mount in read-only place")
   else
-    local instream = (mounta and mounta.fs.open(olfds, mounta.path, "r")) or oldfs.open(patha, "r")
-    local attempt, outstream = pcall(function() return (mountb and mountb.fs.open(oldfs, mountb.path, "r")) or oldfs.open(pathb, "w") end)
-    if attempt then
-      outstream.write(instream.readAll())
-      outstream.close()
-    end
-    instream.close()
+    mounts[#mounts + 1] = {fs = mount, path = path}
   end
 end
-
--- TODOs
-
-fs.find = oldfs.find
-
-fs.complete = oldfs.complete
 
 copyTable(fs, _G.fs)
