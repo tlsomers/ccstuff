9ba4d8e64d4d7a0f381c7d89dbb7b9ca27677bae
Thomas Somers <t.l.somers@student.tue.nl>
2020-07-03 10:16:42 +0200

    Update fileplugger

--- pluggablefs.lua
+++ pluggablefs.lua
@@ -14,7 +14,9 @@ function fs.mount(path, mount)
     error("Must mount in dir")
   elseif oldfs.isReadOnly(path) then
     error("Cannot mountin read-only place")
-  else
+  elseif getMount(path) then
+    error("Cannot mount in mount")
+  end
     mounts[path] = mount
   end
 end
@@ -75,14 +77,71 @@ function fs.combine(a,b)
   return oldfs.combine(a,b)
 end
 
--- TODO
+local function moveOut(mount, dest, totalPath)
+  if mount.fs.moveOut then
+    return mount.fs.moveOut(oldfs, mount.path, dest)
+  else
+    return oldfs.move(totalPath, dest)
+  end
+end
+
+local function moveIn(mount, source, totalPath)
+  if mount.fs.moveIn then
+    return mount.fs.moveIn(oldfs, dest, mount.path)
+  else
+    return oldfs.move(source, totalPath)
+  end
+end
+
+function fs.move(patha, pathb)
+  patha = clean(patha)
+  pathb = clean(pathb)
+  local mounta = getMount(patha)
+  local mountb = getMount(pathb)
+  if not mounta and not mountb then
+    return oldfs.move(patha, pathb)
+  elseif mounta and mountb and mounta.fs == mountb.fs then
+    if mounta.fs.move then
+      return mounta.fs.move(oldfs, mounta.path, mountb.path)
+    else
+      return oldfs.move(patha, pathb)
+    end
+  elseif mounta and not mountb then
+    return moveOut(mounta, pathb, patha)
+  else if mountb and not mounta then
+    return moveIn(mountb, patha, pathb)
+  else
+    local temp = makeTemp()
+    return moveOut(mounta, temp, patha)
+    return moveIn(mountb, temp, pathb)
+  end
+end
+
+function fs.copy (patha, pathb)
+  patha = clean(patha)
+  pathb = clean(pathb)
+  local mounta = getMount(patha)
+  local mountb = getMount(pathb)
+  if not mounta and not mountb then
+    oldfs.copy(patha, pathb)
+  elseif mounta and mountb and mounta.fs == mountb.fs then
+    if mounta.fs.copy then
+      return mounta.fs.copy(oldfs, mounta.path, mountb.path)
+    else
+      return oldfs.copy(patha, pathb)
+    end
+  else
+    local instream = (mounta and mounta.fs.open(olfds, mounta.path, "r")) or oldfs.open(patha, "r")
+    local attempt, outstream = pcall(function() return (mountb and mountb.fs.open(oldfs, mountb.path, "r")) or oldfs.open(pathb, "w") end)
+    if attempt then
+      outstream.write(instream.readAll())
+      outstream.close()
+    end
+    instream.close()
+end
 
 fs.find = oldfs.find
 
 fs.complete = oldfs.complete
-
-fs.move = oldfs.move
-
-fs.copy = oldfs.copy
 
 _G.fs = fs
